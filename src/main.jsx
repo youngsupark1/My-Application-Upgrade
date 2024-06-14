89a652307ffc33e60203d3eed5b932f0e73578b7
youngsupark1 <eddy364885@gmail.com>
2024-06-14 10:22:48 +0900

    main에 QueryClientProvider 작성

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import QueryClientSetup from "./QueryClientSetup.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <QueryClientSetup>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </QueryClientSetup>
+  </QueryClientProvider>
 
   // </React.StrictMode>,
 );
